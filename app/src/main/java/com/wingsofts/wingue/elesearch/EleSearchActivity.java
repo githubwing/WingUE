f9445cdc56a67abf723b53f5c69281664b64790a
lyzqw <327037419@qq.com>
2018-04-11 11:42:54 +0800

    add hint text anim
    Signed-off-by: lyzqw <327037419@qq.com>

--- app/src/main/java/com/wingsofts/wingue/elesearch/EleSearchActivity.java
+++ app/src/main/java/com/wingsofts/wingue/elesearch/EleSearchActivity.java
@@ -1,7 +1,9 @@
 package com.wingsofts.wingue.elesearch;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewTreeObserver;
@@ -12,166 +14,158 @@ import android.widget.TextView;
 import com.wingsofts.wingue.R;
 
 public class EleSearchActivity extends AppCompatActivity {
-    private TextView mSearchBGTxt;
-    private TextView mHintTxt;
-    private TextView mSearchTxt;
-    private FrameLayout mContentFrame;
-    private ImageView mArrowImg;
-    private boolean finishing;
+  private TextView mSearchBGTxt;
+  private TextView mHintTxt;
+  private TextView mSearchTxt;
+  private FrameLayout mContentFrame;
+  private ImageView mArrowImg;
+  private boolean finishing;
+  private float originX;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ele_search);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_ele_search);
+    initView();
+    execute();
+  }
 
-        mSearchBGTxt = (TextView) findViewById(R.id.tv_search_bg);
-        mHintTxt = (TextView) findViewById(R.id.tv_hint);
-        mContentFrame = (FrameLayout) findViewById(R.id.frame_content_bg);
-        mSearchTxt = (TextView) findViewById(R.id.tv_search);
-        mArrowImg = (ImageView) findViewById(R.id.iv_arrow);
+  private void initView() {
+    mSearchBGTxt = (TextView) findViewById(R.id.tv_search_bg);
+    mHintTxt = (TextView) findViewById(R.id.tv_hint);
+    mContentFrame = (FrameLayout) findViewById(R.id.frame_content_bg);
+    mSearchTxt = (TextView) findViewById(R.id.tv_search);
+    mArrowImg = (ImageView) findViewById(R.id.iv_arrow);
+  }
 
-        mSearchBGTxt.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                mSearchBGTxt.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-
-                performEnterAnimation();
-            }
+  private void execute() {
+    mSearchBGTxt.getViewTreeObserver()
+        .addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+          @Override public void onGlobalLayout() {
+            mSearchBGTxt.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            performEnterAnimation();
+          }
         });
+  }
 
+  private void performEnterAnimation() {
+    initLocation();
+    final float top = getResources().getDisplayMetrics().density * 20;
+    final ValueAnimator translateVa = translateVa(mSearchBGTxt.getY(), top);
+    final ValueAnimator scaleVa = scaleVa(1, 0.8f);
+    final ValueAnimator alphaVa = alphaVa(0, 1f);
+    originX = mHintTxt.getX();
+    final float leftSpace = mArrowImg.getRight() * 2;
+    final ValueAnimator translateVaX = translateVax(originX, leftSpace);
 
+    setDuration(translateVa, scaleVa, translateVaX, alphaVa);
+    star(translateVa, scaleVa, translateVaX, alphaVa);
+  }
+
+  private void initLocation() {
+    final float translateY = getTranslateY();
+    //放到前一个页面的位置
+    mSearchBGTxt.setY(mSearchBGTxt.getY() + translateY);
+    mHintTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mHintTxt.getHeight()) / 2);
+    mSearchTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mSearchTxt.getHeight()) / 2);
+  }
+
+  private float getTranslateY() {
+    float originY = getIntent().getIntExtra("y", 0);
+    int[] location = new int[2];
+    mSearchBGTxt.getLocationOnScreen(location);
+    return originY - (float) location[1];
+  }
+
+  @Override public void onBackPressed() {
+    if (!finishing) {
+      finishing = true;
+      performExitAnimation();
     }
+  }
 
-    private void performEnterAnimation() {
-        float originY = getIntent().getIntExtra("y", 0);
+  private void performExitAnimation() {
+    final float translateY = getTranslateY();
+    final ValueAnimator translateVa = translateVa(mSearchBGTxt.getY(), mSearchBGTxt.getY() + translateY);
+    final ValueAnimator scaleVa = scaleVa(0.8f, 1f);
+    final ValueAnimator alphaVa = alphaVa(1f, 0f);
+    exitListener(translateVa);
+    final float currentX = mHintTxt.getX();
+    ValueAnimator translateVaX = translateVax(currentX, originX);
 
-        int location[] = new int[2];
-        mSearchBGTxt.getLocationOnScreen(location);
+    setDuration(translateVa, scaleVa, translateVaX, alphaVa);
+    star(translateVa, scaleVa, translateVaX, alphaVa);
+  }
 
-        final float translateY = originY - (float) location[1];
+  @NonNull private ValueAnimator translateVax(float from, float to) {
+    ValueAnimator translateVaX = ValueAnimator.ofFloat(from, to);
+    translateVaX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override public void onAnimationUpdate(ValueAnimator animation) {
+        Float value = (Float) animation.getAnimatedValue();
+        mHintTxt.setX(value);
+      }
+    });
+    return translateVaX;
+  }
 
-        //放到前一个页面的位置
-        mSearchBGTxt.setY(mSearchBGTxt.getY() + translateY);
+  @NonNull private ValueAnimator translateVa(float from, float to) {
+    ValueAnimator translateVa = ValueAnimator.ofFloat(from, to);
+    translateVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mSearchBGTxt.setY((Float) valueAnimator.getAnimatedValue());
+        mArrowImg.setY(
+            mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mArrowImg.getHeight()) / 2);
         mHintTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mHintTxt.getHeight()) / 2);
-        mSearchTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mSearchTxt.getHeight()) / 2);
-        float top = getResources().getDisplayMetrics().density * 20;
-        final ValueAnimator translateVa = ValueAnimator.ofFloat(mSearchBGTxt.getY(), top);
-        translateVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mSearchBGTxt.setY((Float) valueAnimator.getAnimatedValue());
+        mSearchTxt.setY(
+            mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mSearchTxt.getHeight()) / 2);
+      }
+    });
+    return translateVa;
+  }
 
-                mArrowImg.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mArrowImg.getHeight()) / 2);
-                mHintTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mHintTxt.getHeight()) / 2);
-                mSearchTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mSearchTxt.getHeight()) / 2);
-            }
-        });
+  @NonNull private ValueAnimator scaleVa(float from, float to) {
+    ValueAnimator scaleVa = ValueAnimator.ofFloat(from, to);
+    scaleVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mSearchBGTxt.setScaleX((Float) valueAnimator.getAnimatedValue());
+      }
+    });
+    return scaleVa;
+  }
 
-        ValueAnimator scaleVa = ValueAnimator.ofFloat(1, 0.8f);
-        scaleVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mSearchBGTxt.setScaleX((Float) valueAnimator.getAnimatedValue());
-            }
-        });
+  @NonNull private ValueAnimator alphaVa(float from, float to) {
+    ValueAnimator alphaVa = ValueAnimator.ofFloat(from, to);
+    alphaVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mContentFrame.setAlpha((Float) valueAnimator.getAnimatedValue());
+        mSearchTxt.setAlpha((Float) valueAnimator.getAnimatedValue());
+        mArrowImg.setAlpha((Float) valueAnimator.getAnimatedValue());
+      }
+    });
+    return alphaVa;
+  }
 
-        ValueAnimator alphaVa = ValueAnimator.ofFloat(0, 1f);
-        alphaVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mContentFrame.setAlpha((Float) valueAnimator.getAnimatedValue());
-                mSearchTxt.setAlpha((Float) valueAnimator.getAnimatedValue());
-                mArrowImg.setAlpha((Float) valueAnimator.getAnimatedValue());
-            }
-        });
+  private void exitListener(ValueAnimator translateVa) {
+    translateVa.addListener(new AnimatorListenerAdapter() {
+      @Override public void onAnimationEnd(Animator animation) {
+        finish();
+        overridePendingTransition(0, 0);
+      }
+    });
+  }
 
-        alphaVa.setDuration(500);
-        translateVa.setDuration(500);
-        scaleVa.setDuration(500);
+  private void setDuration(ValueAnimator translateVa, ValueAnimator scaleVa,
+      ValueAnimator translateVaX, ValueAnimator alphaVa) {
+    alphaVa.setDuration(350);
+    translateVa.setDuration(350);
+    scaleVa.setDuration(350);
+    translateVaX.setDuration(350);
+  }
 
-        alphaVa.start();
-        translateVa.start();
-        scaleVa.start();
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (!finishing){
-            finishing = true;
-            performExitAnimation();
-        }
-    }
-
-    private void performExitAnimation() {
-        float originY = getIntent().getIntExtra("y", 0);
-
-        int location[] = new int[2];
-        mSearchBGTxt.getLocationOnScreen(location);
-
-        final float translateY = originY - (float) location[1];
-
-
-        final ValueAnimator translateVa = ValueAnimator.ofFloat(mSearchBGTxt.getY(), mSearchBGTxt.getY()+translateY);
-        translateVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mSearchBGTxt.setY((Float) valueAnimator.getAnimatedValue());
-                mArrowImg.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mArrowImg.getHeight()) / 2);
-                mHintTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mHintTxt.getHeight()) / 2);
-                mSearchTxt.setY(mSearchBGTxt.getY() + (mSearchBGTxt.getHeight() - mSearchTxt.getHeight()) / 2);
-            }
-        });
-        translateVa.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                finish();
-                overridePendingTransition(0, 0);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-
-        ValueAnimator scaleVa = ValueAnimator.ofFloat(0.8f, 1f);
-        scaleVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mSearchBGTxt.setScaleX((Float) valueAnimator.getAnimatedValue());
-            }
-        });
-
-        ValueAnimator alphaVa = ValueAnimator.ofFloat(1, 0f);
-        alphaVa.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                mContentFrame.setAlpha((Float) valueAnimator.getAnimatedValue());
-
-                mArrowImg.setAlpha((Float) valueAnimator.getAnimatedValue());
-                mSearchTxt.setAlpha((Float) valueAnimator.getAnimatedValue());
-            }
-        });
-
-
-        alphaVa.setDuration(500);
-        translateVa.setDuration(500);
-        scaleVa.setDuration(500);
-
-        alphaVa.start();
-        translateVa.start();
-        scaleVa.start();
-
-    }
+  private void star(ValueAnimator translateVa, ValueAnimator scaleVa, ValueAnimator translateVaX,
+      ValueAnimator alphaVa) {
+    alphaVa.start();
+    translateVa.start();
+    scaleVa.start();
+    translateVaX.start();
+  }
 }
